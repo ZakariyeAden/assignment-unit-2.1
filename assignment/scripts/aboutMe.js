21b2d2d9001aac7fa36473dfb86fc2b1b5c6b24f
ZakariyeAden <zakariye5417@gmail.com>
2023-05-11 11:00:19 -0500

    answering the stretch question

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -52,6 +52,13 @@ if(pets < allowedPets){
 }else{
   console.log("Oh no, I have too many pets!");
 }
+
+
+// *****************************************************
+// *********** Extra Stretch or Optional  *********** //
+// *****************************************************
+
+
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
@@ -59,7 +66,13 @@ if(pets < allowedPets){
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
-
+  let mostPets;
+  if(pets > friendsPets){
+    mostPets = pets;
+  } else{
+    mostPets = friendsPets;
+  }
+console.log(mostPets);
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2

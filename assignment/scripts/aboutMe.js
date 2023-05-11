c1e2c01854ce9497126c37198479c676bf29985f
ZakariyeAden <zakariye5417@gmail.com>
2023-05-11 11:08:47 -0500

    finished

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -94,6 +94,6 @@ switch(luckyNumber){
   break;
 }
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-
+adventurous ? console.log("Adventures are great!") : console.log("How about we stay home?");
 
 
